8c824f2bfd7aee47e4e06dfeaeb59f0a4c07daa8
ysnbyzli <yasinbeyazli29@gmail.com>
2021-08-27 21:00:13 +0300

    fix categories item

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const CategoryItem = ({ category }) => {
     return (
-        <a href="#" className="flex justify-center items-center rounded flex-col p-4">
-            <img src={category.image} className="w-12 h-12 rounded-lg border border-red-900 object-cover" />
-            <span className="text-sm font-semibold whitespace-nowrap block mt-2">{category.title}</span>
+        <a href="#" className="flex justify-center items-center rounded flex-col p-4 group hover:bg-purple-50">
+            <img src={category.image} className="w-12 h-12 rounded-lg border border-gray-200 object-cover" />
+            <span className="text-sm font-semibold whitespace-nowrap block mt-2 group-hover:text-purple-700">{category.title}</span>
         </a>
     )
 }
