97c60b54f2b957480efda2a572452ddac7c7bcab
ysnbyzli <yasinbeyazli29@gmail.com>
2021-08-27 23:09:40 +0300

    created footer components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import Campaign from "./components/Campaign";
 import Cards from "./components/Cards";
 import Categories from "./components/Categories";
 import Favorites from "./components/Favorites";
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
 import Mobile from "./components/Mobile";
@@ -16,6 +17,7 @@ function App() {
       <Favorites />
       <Mobile />
       <Cards />
+      <Footer />
     </div>
   );
 }
